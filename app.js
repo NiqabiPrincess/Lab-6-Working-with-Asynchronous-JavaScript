4c4885436c8b6fd551c7f3367596660414b9bcdf
unknown <2002zainabaamir@gmail.com>
2024-11-09 01:17:04 -0500

    made get user content function

--- app.js
+++ app.js
@@ -13,16 +13,16 @@ async function getUserProfile (){
 
     });
 }
-getUserProfile()
+/* getUserProfile()
 .then(profile => console.log("Profile loaded:", profile))  
-.catch(error => console.error(error));  
+.catch(error => console.error(error));  */
 
 async function getPosts(postID){
     return new Promise ((resolve, reject) => {
         let post = true;
-        if (postID === 3) { //* faking an error 
+        /*if (postID === 3) { //* faking an error 
             post = false; 
-          }
+          } */
         console.log(`User's Post ${postID} is Loading...`);
 
         setTimeout(() => {
@@ -35,7 +35,7 @@ async function getPosts(postID){
         }, 1400);
     })
 }
-getPosts(1)
+/* getPosts(1)
   .then(post1 => {
     console.log(post1);  
     return getPosts(2);  
@@ -50,7 +50,7 @@ getPosts(1)
   })
   .catch(error => {
     console.error(error);  
-  });
+  }); */
 
 async function getComments(commentID){
     return new Promise ((resolve, reject) => {
@@ -59,17 +59,17 @@ async function getComments(commentID){
         console.log(`User's comment ${commentID} is Loading...`);
 
         setTimeout(() => {
-            if (comment > 1){ // makes random errors/successes
+            if (comment > 0.15){ // makes random errors/successes
                 resolve(`Comment ${commentID}: content`);
             }
             else{
                 reject(`Error in loading this user's post. Fail to upload comment ${commentID}. `);
             }
-        }, 1500);
+        }, 1250);
     })
 }
 
-Promise.all([getComments(1), getComments(2), getComments(3)])
+/* Promise.all([getComments(1), getComments(2), getComments(3)])
  .then(results => {
     results.forEach(comment => {
         console.log(comment);
@@ -77,6 +77,36 @@ Promise.all([getComments(1), getComments(2), getComments(3)])
  })
  .catch(error => {
     console.error(error);
- });
+ }); */
+
+async function getUserContent() {
+    try {
+        const profile = await getUserProfile();
+        console.log("User Profile Loaded:1", profile);
+
+        const post1 = await getPosts(1);
+        console.log("Post 1 Loaded:", post1);
+        const post2 = await getPosts(2);
+        console.log("Post 2 Loaded:", post2);
+        const post3 = await getPosts(3);
+        console.log("Post 3 Loaded:", post3);
+
+        const comment1 = await getComments(1);
+        console.log("Comment 1 Loaded:", comment1);
+        const comment2 = await getComments(2);
+        console.log("Comment 2 Loaded:", comment2);
+        const comment3 = await getComments(3);
+        console.log("Comment 3 Loaded:", comment3);
+    }
+    
+    catch(error){
+        console.error("Error:", error);
+    }
+    
+}
+getUserContent();
+
+
+
 
 
