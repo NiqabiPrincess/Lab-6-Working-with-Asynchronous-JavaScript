47eaf05efaa2dbd0b1cb4013c7855dce2fc59518
unknown <2002zainabaamir@gmail.com>
2024-11-08 23:21:32 -0500

    getting basics set up

---
+++ app.js
@@ -0,0 +1,19 @@
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+function getUserProfile (){
+    
+
+}
+
+function getPosts(){
+
+
+}
+
+function getComments(){
+
+    
+}
+
