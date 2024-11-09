9b5da832724698f00a27cb47c0f3a3d13b779fa3
unknown <2002zainabaamir@gmail.com>
2024-11-09 00:26:42 -0500

    implemented sequential data fetching for getPost function, with an error message for post 3

--- app.js
+++ app.js
@@ -17,23 +17,40 @@ getUserProfile()
 .then(profile => console.log("Profile loaded:", profile))  
 .catch(error => console.error(error));  
 
-function getPosts(){
+async function getPosts(postId){
     return new Promise ((resolve, reject) => {
         let post = true;
-        console.log("User's Posts Loading...");
+        if (postId === 3) { //* faking an error 
+            post = false; 
+          }
+        console.log(`User's Post ${postId} is Loading...`);
 
         setTimeout(() => {
             if (post){
                 resolve(`Post ${postId}: content`);
             }
             else{
-                reject(`Error in loading this user's post. Post is ${post}. Fail to upload post ${postId}.`);
+                reject(`Error in loading this user's post. Fail to upload post ${postId}. Post is ${post}. `);
             }
-
         }, 1000);
     })
-
 }
+getPosts(1)
+  .then(post1 => {
+    console.log(post1);  
+    return getPosts(2);  
+  })
+  .then(post2 => {
+    console.log(post2);  
+    return getPosts(3);
+    
+  })
+  .then(post3 => {
+    console.log(post3);  //* should show the error message
+  })
+  .catch(error => {
+    console.error(error);  
+  });
 
 async function getComments(){
 
